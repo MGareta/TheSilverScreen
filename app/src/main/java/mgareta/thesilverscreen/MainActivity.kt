e634cf745fe1c8371d1a9e88dda72c3b92ef290b
MGareta <marc.gareta@outlook.com>
2018-11-07 12:16:52 +0100

    Custom toolbar

--- app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
+++ app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
@@ -2,6 +2,7 @@ package mgareta.thesilverscreen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.main_activity.*
 import mgareta.thesilverscreen.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        setSupportActionBar(toolbar_main)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())

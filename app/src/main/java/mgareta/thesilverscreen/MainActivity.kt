089910c72b9202375e48ab38495f010e90e7d000
MGareta <marc.gareta@outlook.com>
2018-11-08 15:36:22 +0100

    Config the BoomMenu to show all the endpoints on click

--- app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
+++ app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
@@ -1,9 +1,14 @@
 package mgareta.thesilverscreen
 
+import android.content.res.TypedArray
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.nightonke.boommenu.BoomButtons.HamButton
+import com.nightonke.boommenu.Util
 import kotlinx.android.synthetic.main.main_activity.*
 import mgareta.thesilverscreen.ui.main.MainFragment
+import mgareta.thesilverscreen.ui.main.listeners.EndPointChooseListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,9 +29,35 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        configEndPointChooser()
     }
 
     override fun onPause() {
+        boomMenuButton_toolbar.clearBuilders()
+
         super.onPause()
+    }
+
+    private fun configEndPointChooser() {
+        val endPointsIcons: TypedArray = resources.obtainTypedArray(R.array.toolbar_endPointEntriesIcons)
+        val endPointsTitleEntries = resources.getStringArray(R.array.toolbar_endPointEntriesTitle)
+        val endPointsSubtitleEntries = resources.getStringArray(R.array.toolbar_endPointEntriesSubtitle)
+
+        for (i in 0 until boomMenuButton_toolbar.piecePlaceEnum.pieceNumber()) {
+            val builder = HamButton.Builder()
+                .normalColorRes(R.color.colorAccent)
+                .highlightedColor(R.color.ripple_material_light)
+                .normalImageDrawable(endPointsIcons.getDrawable(i))
+                .normalText(endPointsTitleEntries[i])
+                .subNormalText(endPointsSubtitleEntries[i])
+                .imagePadding(Rect(Util.dp2px(15f),0,Util.dp2px(5f),0))
+                .rotateImage(false)
+                .listener(EndPointChooseListener())
+            boomMenuButton_toolbar.addBuilder(builder)
+        }
+
+        endPointsIcons.recycle()
+
+        textView_boomMenuButton.text = endPointsTitleEntries[0].toString()
     }
 }

0454753693a230abf1bc2d17a012ac3f2580072c
MGareta <marc.gareta@outlook.com>
2018-11-08 11:44:23 +0100

    Remove default spinner from toolbar

--- app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
+++ app/src/main/java/mgareta/thesilverscreen/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.main_activity.*
 import mgareta.thesilverscreen.ui.main.MainFragment
-import mgareta.thesilverscreen.ui.main.listeners.SpinnerListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,12 +24,9 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        spinner_toolbar.onItemSelectedListener = SpinnerListener()
     }
 
     override fun onPause() {
-        spinner_toolbar.onItemSelectedListener = null
-
         super.onPause()
     }
 }
